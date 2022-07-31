ef6cc72c5bd7673c3b6f4459231aeab3855acc1b
Quyenlt <quyenlt08012001@example.com>
2022-07-31 11:27:54 +0700

    update

--- src/main/java/com/swp/SchoolManagement/Repository/ClassRepository.java
+++ src/main/java/com/swp/SchoolManagement/Repository/ClassRepository.java
@@ -10,7 +10,7 @@ import com.swp.SchoolManagement.model.Class;
 
 @Repository
 public interface ClassRepository extends JpaRepository<Class, Long>{
-    String sql = "select distinct( c.id),c.class_name from Timetable t "+
+    String sql = "select distinct( c.class_id),c.class_name from Timetable t "+
     "join Class c on c.class_id = t.class_id        "+
     "join DaySlot d on t.day_slot_id = d.id         "+
     "where subject_id = :subjectId   ";
